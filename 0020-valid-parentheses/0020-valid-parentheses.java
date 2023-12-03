d30af16e7e413848df14124110d76398b0cebce7
Mayar Adel <48599480+Mayaradel@users.noreply.github.com>
2023-12-03 03:29:15 +0200

    Time: 2 ms (82.91%), Space: 40.6 MB (32.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char bracket : s.toCharArray()) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+            } else {
+                if (stack.isEmpty()) return false;
+            
+            char openBracket = stack.pop();
+
+            if ((bracket == ')' && openBracket != '(') || (bracket == '}' && openBracket != '{') || (bracket == ']' && openBracket != '[')) return false;
+        }
+        }
+        return stack.isEmpty();
+    }
+}
