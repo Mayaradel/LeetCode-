55ea387d2338d637a49abfbf5807b3d594df10f8
Mayar Adel <48599480+Mayaradel@users.noreply.github.com>
2023-12-03 03:30:38 +0200

    Time: 11 ms (55.66%), Space: 55 MB (64.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null)
+        return false;
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num))
+            return true;
+
+            set.add(num);
+        }
+        return false;
+      
+}
+}
